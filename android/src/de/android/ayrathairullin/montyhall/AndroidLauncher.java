c7af4bf04733ddab670598d219f7aa0b8f968aab
Ayrat <ayrat1@mail.ru>
2018-03-28 09:40:57 +0300

    Initial commit

---
+++ android/src/de/android/ayrathairullin/montyhall/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.android.ayrathairullin.montyhall;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.android.ayrathairullin.montyhall.Monty;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Monty(), config);
+	}
+}
---
+++ desktop/src/de/android/ayrathairullin/montyhall/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.android.ayrathairullin.montyhall.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.android.ayrathairullin.montyhall.Monty;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Monty(), config);
+	}
+}
